01537bc7c89ddd29b01054fcc9a694f660669c0f
Nola J. Gordon <njg2@tcl216-18.cs.williams.edu>
2016-01-08 11:44:01 -0500

    updated persistence, added mixins

--- assets/entity.js
+++ assets/entity.js
@@ -11,27 +11,35 @@ Game.Entity = function(template) {
 
   // mixin sutff
   // track mixins and groups, copy over non-META properties, and run the mixin init if it exists
+  this._mixins = template.mixins || [];
   this._mixinTracker = {};
   console.dir(template);
   console.dir(template.mixins);
-  if (template.hasOwnProperty('mixins')) {
-    for (var i = 0; i < template.mixins.length; i++) {
-      var mixin = template.mixins[i];
-      console.dir(mixin);
-      this._mixinTracker[mixin.META.mixinName] = true;
-      this._mixinTracker[mixin.META.mixinGroup] = true;
-      for (var mixinProp in mixinProp != 'META' && mixin) {
-        if (mixinProp != 'META' && mixin.hasOwnProperty(mixinProp)) {
-          this[mixinProp] = mixin[mixinProp];
-        }
+  console.dir(this._mixins);
+  for (var i = 0; i < this._mixins.length; i++) {
+    var mixin = this._mixins[i];
+    console.dir(mixin);
+    this._mixinTracker[mixin.META.mixinName] = true;
+    this._mixinTracker[mixin.META.mixinGroup] = true;
+    for (var mixinProp in mixinProp != 'META' && mixin) {
+      if (mixinProp != 'META' && mixin.hasOwnProperty(mixinProp)) {
+        this[mixinProp] = mixin[mixinProp];
       }
-      if (mixin.META.hasOwnProperty('init')) {
-        mixin.META.init.call(this,template);
-      }
+    }
+    if (mixin.META.hasOwnProperty('init')) {
+      mixin.META.init.call(this,template);
     }
   }
 };
 Game.Entity.extend(Game.Symbol);
+
+Game.Entity.prototype.hasMixin = function(checkThis) {
+  if (typeof checkThis == 'object') {
+    return this._mixinTracker.hasOwnProperty(checkThis.META.mixinName);
+  } else {
+    return this._mixinTracker.hasOwnProperty(checkThis);
+  }
+};
 
 Game.Entity.prototype.getName = function() {
   return this.attr._name;
@@ -57,26 +65,21 @@ Game.Entity.prototype.getY   = function() {
 };
 
 Game.Entity.prototype.toJSON = function () {
-  var json = {};
-  for (var at in this.attr) {
-    if (this.attr.hasOwnProperty(at)) {
-      if (this.attr[at] instanceof Object && 'toJSON' in this.attr[at]) {
-        json[at] = this.attr[at].toJSON();
-      } else {
-        json[at] = this.attr[at];
-      }
-    }
-  }
+  var json = Game.UIMode.gamePersistence.BASE_toJSON.call(this);
+  // for (var i = 0; i < this._mixins; i++) {
+  //   var mixin = this._mixins[i];
+  //   if (mixin.META.toJSON) {
+  //     json['mixin:'+mixin.META.mixinName] = mixin.META.toJSON.call(this);
+  //   }
+  // }
   return json;
 };
 Game.Entity.prototype.fromJSON = function (json) {
-  for (var at in this.attr) {
-    if (this.attr.hasOwnProperty(at)) {
-      if (this.attr[at] instanceof Object && 'fromJSON' in this.attr[at]) {
-        this.attr[at].fromJSON(json[at]);
-      } else {
-        this.attr[at] = json[at];
-      }
-    }
-  }
+  Game.UIMode.gamePersistence.BASE_fromJSON.call(this,json);
+  // for (var i = 0; i < this._mixins; i++) {
+  //   var mixin = this._mixins[i];
+  //   if (mixin.META.fromJSON) {
+  //     mixin.META.fromJSON.call(this,json['mixin:'+mixin.META.mixinName]);
+  //   }
+  // }
 };
